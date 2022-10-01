75149e590a887378dfddc24ac291de4c093f6b33
JD <cogscidawg@gmail.com>
2022-10-01 15:13:46 -0400

    -added context

--- practice/views.py
+++ practice/views.py
@@ -6,6 +6,13 @@ from django.views.generic import TemplateView
 class FilterView(TemplateView):
     template_name = 'practice/filters.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['company'] = 'Webucator'
+        context['url'] = 'https://www.webucator.com'
+
+        return context
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -13,3 +20,16 @@ class HomePageView(TemplateView):
 
 class TagView(TemplateView):
     template_name = 'practice/tags.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['beatles'] = [
+        {'firstname': 'Paul', 'lastname': 'McCartney'},
+        {'firstname': 'John', 'lastname': 'Lennon'},
+        {'firstname': 'George', 'lastname': 'Harrison'},
+        {'firstname': 'Ringo', 'lastname': 'Starr'},
+        ]
+    
+        return context
+
+
