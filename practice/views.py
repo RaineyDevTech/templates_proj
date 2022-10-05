b54d0f18a5e794cd2cf8fdd8049c1d677fda0d4a
JD <cogscidawg@gmail.com>
2022-10-04 21:43:57 -0400

    -template filters added

--- practice/views.py
+++ practice/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
 from django.views.generic import TemplateView
 
 class FilterView(TemplateView):
@@ -10,13 +9,16 @@ class FilterView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['company'] = 'Webucator'
         context['url'] = 'https://www.webucator.com'
+        context['moon_landing'] = datetime.datetime(
+            year=1969, month=7, day=21,
+            hour=2, minute=56, second=15,
+            tzinfo=datetime.timezone.utc
+        )
 
         return context
 
-
 class HomePageView(TemplateView):
     template_name = 'home.html'
-
 
 class TagView(TemplateView):
     template_name = 'practice/tags.html'
@@ -24,12 +26,10 @@ class TagView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['beatles'] = [
-        {'firstname': 'Paul', 'lastname': 'McCartney'},
-        {'firstname': 'John', 'lastname': 'Lennon'},
-        {'firstname': 'George', 'lastname': 'Harrison'},
-        {'firstname': 'Ringo', 'lastname': 'Starr'},
+            {'firstname': 'Paul', 'lastname': 'McCartney'},
+            {'firstname': 'John', 'lastname': 'Lennon'},
+            {'firstname': 'George', 'lastname': 'Harrison'},
+            {'firstname': 'Ringo', 'lastname': 'Starr'},
         ]
-    
+        
         return context
-
-
